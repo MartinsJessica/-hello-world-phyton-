d18bab962136d3b3810fde7a640dfea3d415a137
MartinsJessica <101491038+MartinsJessica@users.noreply.github.com>
2023-02-28 22:12:45 -0300

    Create idade.py

---
+++ idade.py
@@ -0,0 +1,9 @@
+idade = int(input("Digite sua idade: "))
+if (idade >=18) : 
+    print("Vc é de MAIOR")
+    print("Voce pode tirar carta")
+
+else:
+
+    print("Voce é de menor")
+print("Fim do programa")
