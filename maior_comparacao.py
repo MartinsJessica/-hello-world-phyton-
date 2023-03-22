c1a18f79c40a860f8708349856a539c5093c1aa0
MartinsJessica <101491038+MartinsJessica@users.noreply.github.com>
2023-03-21 21:45:28 -0300

    Create maior_comparacao.py

---
+++ maior_comparacao.py
@@ -0,0 +1,7 @@
+primeiro_numero = float(input("Informe um número:"))
+segundo_numero = float(input("Agora, outro número:"))
+
+if (primeiro_numero > segundo_numero):
+    print(primeiro_numero, " é maior que", segundo_numero)
+else:
+    print(segundo_numero, " é maior que", primeiro_numero)
