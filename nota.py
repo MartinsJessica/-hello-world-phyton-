ad66b08315bbbf22a25e0024b229816f6cd1dca3
MartinsJessica <101491038+MartinsJessica@users.noreply.github.com>
2023-03-21 21:37:07 -0300

    Create nota.py

---
+++ nota.py
@@ -0,0 +1,10 @@
+Nota1 = float(input("nota")) 
+Nota2 = float(input("nota"))
+Nota3 = float(input("nota"))
+media =((Nota1+Nota2+Nota3)/3)
+
+print(media)
+if (media >=6):
+    print("Aluno aprovado")
+else: 
+    print("Aluno reprovado")
