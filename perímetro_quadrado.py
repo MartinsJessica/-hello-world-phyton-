6b7ca6c2d30f5f8cc5d56c036ea5580e9fe01cca
MartinsJessica <101491038+MartinsJessica@users.noreply.github.com>
2023-03-21 21:36:20 -0300

    Create perímetro_quadrado.py

---
+++ perímetro_quadrado.py
@@ -0,0 +1,6 @@
+medb = float(input("digite a base"))
+meda = float(input("Digite a altura"))
+areaquadr = medb*meda
+perquadr = medb*4
+
+print("O objeto é uma quadrado, sua área é",areaquadr,"m² e seu perímetro é",perquadr,"m.")
