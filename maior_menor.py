878cbde79c2ed6a40b2e0375cc25200f444cc7ea
MartinsJessica <101491038+MartinsJessica@users.noreply.github.com>
2023-03-21 21:48:30 -0300

    Create maior_menor.py

---
+++ maior_menor.py
@@ -0,0 +1,9 @@
+primeiro_numero = float(input("Número:"))
+segundo_numero = float(input("Número:"))
+terceiro_numero = float(input("Número:"))
+
+maior_numero = max(primeiro_numero, segundo_numero, terceiro_numero)
+menor_numero = min(primeiro_numero, segundo_numero, terceiro_numero)
+
+print("Maior número:", maior_numero)
+print("Menor número:", menor_numero)
